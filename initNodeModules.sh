143b33b0a054a386d41d7e88e0c6603ebe98423e
takahito.miyamoto <takahito.miyamoto@accenture.com>
2020-01-30 00:01:46 +0900

    :zap: node modules

--- initNodeModules.sh
+++ initNodeModules.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+echo
+echo ----------------------------------------
+echo "install sfdx and yarn globally"
+echo ----------------------------------------
+npm install -g sfdx-cli yarn
+
+echo
+echo ----------------------------------------
+echo "install sfpowerkit"
+echo ----------------------------------------
+echo "y" | sfdx plugins:install sfpowerkit
+
+echo
+echo ----------------------------------------
+echo "update devDependencies"
+echo ----------------------------------------
 yarn add --dev --exact \
 @babel/core \
 @babel/register \
@@ -13,3 +29,9 @@ husky \
 lint-staged \
 prettier \
 prettier-plugin-apex
+
+echo
+echo ----------------------------------------
+echo "install node_modules"
+echo ----------------------------------------
+yarn install
